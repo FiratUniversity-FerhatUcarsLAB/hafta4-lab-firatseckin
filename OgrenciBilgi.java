561d068bd3cb775d5c34ffdc09ad695549a64a46
firatseckin <250541042@firat.edu.tr>
2025-10-25 23:50:40 +0300

    Update student information program with details

--- OgrenciBilgi.java
+++ OgrenciBilgi.java
@@ -1,9 +1,9 @@
 /*
- * Ad Soyad: [ADINIZI BURAYA YAZIN]
- * Ogrenci No: [OGRENCI NUMARANIZI BURAYA YAZIN]
- * Tarih: [TARIHI BURAYA YAZIN]
+ * Ad Soyad: [Fırat Seçkin]
+ * Ogrenci No: [250541042]
+ * Tarih: 25.10.2025
  * Aciklama: Gorev 1 - Ogrenci Bilgi Sistemi
- * 
+ *
  * Bu program kullanicidan ogrenci bilgilerini alir ve
  * duzenli bir formatta ekrana yazdirir.
  * Diğer java dosyalarının başında da bu örnek formattaki gibi kısa bilgi giriniz
@@ -14,12 +14,10 @@ import java.util.Scanner;
 public class OgrenciBilgi {
     public static void main(String[] args) {
         // Scanner objesi olusturun
-        
+        Scanner input = new Scanner(System.in);
         
         // Degisken tanimlamalari
-        // String ad, soyad;
-        // int ogrenciNo, yas;
-        // double gpa;
+        // Değişkenleri aşağıda veriyi alırken tanımlayacağız.
         
         
         // Kullanicidan bilgileri alin
@@ -27,34 +25,47 @@ public class OgrenciBilgi {
         System.out.println();
         
         // Ad
-        
+        System.out.print(" adınızı girin: ");
+        String ad = input.next();
         
         // Soyad
-        
+        System.out.print("soyadınızı girin: ");
+        String soyad = input.next();
         
         // Ogrenci No
-        
+        System.out.print("Öğrenci numaranızı girin: ");
+        int ogrenciNo = input.nextInt();
         
         // Yas
-        
+        System.out.print(" yaşınızı girin: ");
+        int yas = input.nextInt();
         
         // GPA
+        System.out.print("GPA (not ortalaması, 0.00-4.00 arası) girin: ");
+        double gpa = input.nextDouble();
         
         
         // Bilgileri ekrana yazdirin
         System.out.println("\n=== OGRENCI BILGI SISTEMI ===");
         
         // COZUMUNUZU BURAYA YAZIN
+        // printf() ile düzenli çıktı alıyoruz
+        System.out.printf("Ad Soyad: %s %s\n", ad, soyad);
+        System.out.printf("Ogrenci No: %1d\n", ogrenciNo);
+        System.out.printf("Yas: %1d\n", yas);
         
-        
-        
-        
-        
-        
+        // GPA'yi 2 ondalık basamakla göstermek için %.2f kullandık
+        System.out.printf("GPA: %.2f\n", gpa);
 
+        // Örnek çıktıdaki "Durum" kısmını da ekleyelim
+        if (gpa >= 2.0) {
+            System.out.println("Durum: Basarili Ogrenci");
+        } else {
+            System.out.println("Durum: Basarisiz Ogrenci");
+        }
         
         
         // Scanner'i kapatin (önemli pratik)
-        
+        input.close();
     }
 }
